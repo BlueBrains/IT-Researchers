0fed1fa8fb06289a837d21da1ec43ff171be495d
abnud1 <ahmad13932013@hotmail.com>
2015-12-04 23:28:52 +0200

    تمت ترجمة الواجهات

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  protected
+
+  def after_sign_in_path_for(researcher)
+   researcher_path(current_researcher)
+  end
+  def after_sign_out_path_for(researcher)
+   'researchers/sign_up'
+  end
+  def after_sign_up_path_for(researcher)
+   researcher_path(current_researcher)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   
-   
-
-  devise_for :researchers, controllers: { sessions: "researchers/sessions", registrations: "researchers" }
+  as :researcher do   
+     root :to => "researchers#new"
+  end
+  devise_for :researchers, controllers: { sessions: "researchers/sessions", registrations: "researchers" }  
+  resources :researchers
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
