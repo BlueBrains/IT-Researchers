e8a25217d42a440b34a6ea2017de65afad0042d5
Molham Al-Maleh <molhamaleh@gmail.com>
2016-01-06 09:17:54 +0200

    added most viewed to home page, related papers to show page

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -25,6 +25,14 @@ class Paper
   field :likes, type: Integer
   field :times_seen, type: Integer
   taggable_with :tags
+  taggable_with :keywords
+
+  scope :exclude, -> paper { ne(_id: paper.id) }
+  scope :similar_to, -> paper { exclude(paper).in(tags: paper.tags ) }
+
+  def similar
+    @similar ||= self.class.similar_to self
+  end
 
   validates_presence_of :title, :abstract, :introduction, :valuation, :conclusion
   #  searchkick language: "arabic"
