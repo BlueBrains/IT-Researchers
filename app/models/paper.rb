e03249c7ad0159757cad296b98fb2905aae0ede7
Molham Al-Maleh <molhamaleh@gmail.com>
2016-01-06 07:19:59 +0200

    added paper fields to model

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -5,14 +5,28 @@ class Paper
   include Mongoid::Commentable  
   has_and_belongs_to_many :researchers  
   belongs_to :category
+  
   field :title
-  field :content
+  field :abstract
+  field :introduction
+  field :literature_survey
+  field :notation
+  field :theory
+  field :specification
+  field :implementation
+  field :valuation
+  field :related_work
+  field :further_work
+  field :conclusion
+  field :appendices
+  
   field :state
   field :likers, type: Array
   field :likes, type: Integer
   field :times_seen, type: Integer
-  validates_presence_of :title  
   taggable_with :tags
+
+  validates_presence_of :title, :abstract, :introduction, :valuation, :conclusion
   #  searchkick language: "arabic"
 end
 
